061f107cea24905b0eefb6c557101d357a7d9749
harixtg <150332613+harixtg@users.noreply.github.com>
2023-11-12 20:11:45 +0530

    Update users_settings.py

--- bot/modules/users_settings.py
+++ bot/modules/users_settings.py
@@ -150,9 +150,10 @@ async def user_settings(client, message):
     thumbnail = f"Thumbnails/{user_id}.jpg"
     if not ospath.exists(thumbnail):
         thumbnail = 'https://graph.org/file/4a23820398e62ec753cc0.jpg'
-    myrr = await message.reply_sticker("CAACAgIAAxkBAAEIK1lkFAN0BjHbiwRY08v-7EFYRqI2fQACKRgAAhP_2UkVxgiD_rlLGS8E")
+    myrr = await message.reply_sticker("CAACAgIAAxkBAAEMymtlUOMCEYuaeaRXYN8vW1J8bnm2NAACrQ0AAqyZIEjdinfy_Yf5cB4E")
     x = await sendMessage(message, msg, button, thumbnail)
     await five_minute_del(message)
+    await asyncio.sleep(2)
     await deleteMessage(x)
     await myrr.delete()
     await message.delete()
