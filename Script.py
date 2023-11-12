816245c1b2c9828a4aaae051f47d79f6721897fe
harixtg <150332613+harixtg@users.noreply.github.com>
2023-11-12 19:42:15 +0530

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,12 @@
+#hari codes
+
+class script(object):
+  
+    USER_SETTINGS_TEXT = """<b><u>🌟 ᴜsᴇʀ sᴇᴛᴛɪɴɢs</u></b>
+    
+<b>┌ ɴᴀᴍᴇ : {}</b>
+<b>├ ᴜsᴇʀ ɴᴀᴍᴇ :</b>{} ( {} )
+<b>├ ʟᴀɴɢᴜᴀɢᴇ :</b> {}
+<b>└ ᴛᴇʟᴇɢʀᴀᴍ ᴅᴄ :</b> {}
+
+<b>🫅 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ » <a href='https://t.me/Hari_OP'>ʜᴀʀɪ ᠰ ᴛɢ​</a></b>"""
